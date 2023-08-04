c17c0dfb9331ffcf40876d00bc09610f1277419a
mostafa <mostafaahmedmaa1@gmail.com>
2023-08-04 15:58:12 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
-const { nextui } = require("@nextui-org/react");
-
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
  
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -18,6 +15,5 @@ module.exports = {
   plugins: [
     require('tailwind-scrollbar'),
     require('@tailwindcss/line-clamp'),
-    nextui()
   ],
 }
