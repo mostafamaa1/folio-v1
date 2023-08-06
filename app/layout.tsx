790435b47d439fc3951d2ce1501c05cffac78aa0
M1Dev <mostafaahmedmaa1@gmail.com>
2023-08-06 21:13:47 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "ABOHADID",
     description:
-      "Film Production management and Modeling. Based in Saudi Arabia.",
+      "Film Production services and Modeling management. Based in Saudi Arabia",
     url: "https://www.abohadid.com",
     siteName: "www.abohadid.com",
     images: [
